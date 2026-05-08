92dd32ea53bcd4c79e69430147f526b729c024eb
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-05-07 19:50:04 -0500

    Initial Commit

---
+++ Tic Tac Toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe
+//
+//  Created by Pamela VanLeirsburg on 5/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
